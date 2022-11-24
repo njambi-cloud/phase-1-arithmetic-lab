f5d35a1cfc02992d16ffdd0a6384a75741cc7ff1
Christine <njambs@Christines-MacBook-Pro.local>
2022-11-24 22:23:59 +0300

    assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+{
+let num1 = 31;
+let num2 = 2;
+const multiply= num1* num2;
+console.log(num1*num2)
+}
+
+let random = Number.isInteger(Math.random() * 10) +1;
+
+let num3 = 25;
+let num4 = 7;
+let mod = (num3 % num4) 
+
+let max = Math.max(1,15,9,17,20)
+max;
+
+ 
+
